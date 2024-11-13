1e077fd94cbcdbde0a6121975c115d5090ec43ad
henriqueess <henrisoares2007@gmail.com>
2024-11-13 11:29:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,131 @@
+criaCartao(
+    'história',
+    'Quem foi o primeiro imperador do Brasil?',
+    'Dom Pedro I'
+)
+
+criaCartao(
+    'história',
+    'Em que ano o Brasil aboliu a escravidão?',
+    '1888'
+)
+
+criaCartao(
+    'história',
+    'Qual foi o marco de independencia do Brasil?',
+    'O grito do ipiranga em 1822'
+)
+
+criaCartao(
+    'história',
+    'Quem liderou a inconidência Mineira?',
+    'Tiradentes'
+)
+criaCartao(
+    'história',
+    'Quando aconteceu a Proclamação da República?',
+    '15 de novembro de 1889'
+)
+criaCartao(
+    'Cultura pop',
+    'Qual o nome da nave do han solo em Star wars?',
+    'Millenium Falcon'
+)
+criaCartao(
+    'Ciencias',
+    'Qual a invenção do Nikola Tesla foi fundamental para a distribuição de energia eletrica?',
+    'Sistema de corrente alternada'
+)
+criaCartao(
+    'Cultura pop',
+    'Qual é o personagem do genero e super-heroi que foi mais adaptado?',
+    'Superman'
+)
+criaCartao(
+    'Cultura pop',
+    'Quem escreveu "Romeu e Julieta"?',
+    'William Shakespeare'
+)
+criaCartao(
+    'História',
+    'Quem pintou o teto da Capela Sistina?',
+    'Michelangelo'
+)
+criaCartao(
+    'Cultura pop',
+    'Quem é conhecido como REI DO POP?',
+    'Michael Jackson'
+)
+criaCartao(
+    'Cultura pop',
+    'Qual foi o nome do albúm de estreia dos beatles?',
+    'Please Please me '
+)
+criaCartao(
+    'Cultura pop',
+    'Em que país nasceu Bob Marley?',
+    'Jamaica'
+)
+criaCartao(
+    'Cultura pop',
+    'Quem foi o rapper que invadiu o grammy?',
+    'Kanye West'
+)
+criaCartao(
+    'Cultura pop',
+    'Quem foi o produtor musical que foi preso por agressão de mulheres e festas ilegais?',
+    'P. Diddy'
+)
+criaCartao(
+    'Cultura pop',
+    'Quando foi lançado o último episódio de supernatural?',
+    'Foi lançado em 19 de novembro e 2020'
+)
+criaCartao(
+    'Cultura pop',
+    'No filme "Como perder um homem em 10 dias" qual era a cor do vestido da Andie na festa?',
+    'A cor amarela'
+)
+criaCartao(
+    'Cultura pop',
+    'Qual a quantidade de filmes na frânquia "Jurrasic Park"?',
+    '6 Filmes'
+)
+criaCartao(
+    'Cultura pop',
+    'Qual é o filme com maior bilheteria na história?',
+    'Avatar (2009) no valor de US$2,847 Bilhões'
+)
+criaCartao(
+    'Cultura pop',
+    'Quantos filmes tem no universo cinematográfrico da Marvel?',
+    'Tem 34 filmes até o momento'
+    )
+    criaCartao(
+        'Cultura pop',
+        'Qual é o Inicial do tipo fogo de pokemon?',
+        'Charmander'
+        )
+        criaCartao(
+            'Cultura pop',
+            'Qual é o pokemon criador do mar?',
+            'Kyogre'
+            )
+            criaCartao(
+                'Cultura pop',
+                'Qual é o personagem protagonista de pokemon?',
+                'Ash'
+                )
+                criaCartao(
+                    'Cultura pop',
+                    'Qual é o nivel máximo de pokémon go?',
+                    '50'
+                )
+                
+            
+        
+    
+
+
+
+
